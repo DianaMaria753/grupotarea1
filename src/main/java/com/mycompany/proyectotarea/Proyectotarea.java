483e3eb7adc609cdef4db830f7cbb550d7bdf020
IvanLmm <ivanlmmaradiaga@gmail.com>
2025-03-04 21:35:07 -0600

    Update Proyectotarea.java
    he removido todas las tildes para que se ejecute correctamente en todos los IDE posibles sin ningun conflicto

--- src/main/java/com/mycompany/proyectotarea/Proyectotarea.java
+++ src/main/java/com/mycompany/proyectotarea/Proyectotarea.java
@@ -5,10 +5,10 @@ public class Proyectotarea {
 
     public static void main(String[] args) {
         // Crear un objeto de la clase Deportista
-        Deportista jugador = new Deportista("Lionel Messi", 36, "Fútbol", "Profesional");
+        Deportista jugador = new Deportista("Lionel Messi", 36, "Futbol", "Profesional");
 
         // Mostrar información del deportista
-        System.out.println("=== Información del Deportista ===");
+        System.out.println("=== Informacion del Deportista ===");
         jugador.mostrarInformacion();
 
         // Simular un entrenamiento
@@ -16,12 +16,12 @@ public class Proyectotarea {
         jugador.entrenar();
 
         // Modificar algunos atributos
-        System.out.println("\n=== Modificación de datos ===");
+        System.out.println("\n=== Modificacion de datos ===");
         jugador.setDeporte("Padel");
         jugador.setNivelCompetencia("Aficionado");
 
         // Mostrar la información actualizada
-        System.out.println("\n=== Información actualizada ===");
+        System.out.println("\n=== Informacion actualizada ===");
         jugador.mostrarInformacion();
     }
 
